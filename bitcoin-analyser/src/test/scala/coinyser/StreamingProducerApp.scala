678affdd22134a2d3083058b0c91c1c1941524a2
Mikael Valot <mikael.valot@gmail.com>
2018-09-12 18:48:44 +0100

    bitcoin analyser - streaming cleanup

--- bitcoin-analyser/src/test/scala/coinyser/StreamingProducerApp.scala
+++ bitcoin-analyser/src/test/scala/coinyser/StreamingProducerApp.scala
@@ -1,33 +1,18 @@
 package coinyser
 
-import java.util.Properties
+import cats.effect.{ExitCode, IO, IOApp}
 import com.pusher.client.Pusher
-import com.pusher.client.connection.ConnectionEventListener
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.receiver.Receiver
-import com.pusher.client.channel.SubscriptionEventListener
-import com.pusher.client.connection.ConnectionState
-import com.pusher.client.connection.ConnectionStateChange
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-
-import scala.concurrent.duration._
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.Producer
-import org.apache.kafka.clients.producer.ProducerRecord
-
-import scala.collection.JavaConversions._
 
 // Use log compaction to ensure high availability ? (2 producers running concurrently)
-// TODO use IOApp
-object StreamingProducerApp extends App {
-  val config = KafkaConfig("localhost:9092", "transactions_draft4")
+object StreamingProducerApp extends IOApp {
 
-  val pusher = new Pusher("de504dc5763aeef9ff52")
-  StreamingProducer.start(pusher, config).unsafeRunSync()
+  def run(args: List[String]): IO[ExitCode] = {
+    val config = KafkaConfig("localhost:9092", "transactions_draft4")
 
-  // TODO is there an IO solution for that ?
-  Thread.sleep(Long.MaxValue)
+    val pusher = new Pusher("de504dc5763aeef9ff52")
+    StreamingProducer.start(pusher, config).map(_ => ExitCode.Success)
+
+  }
 }
 
 
--- bitcoin-analyser/src/test/scala/coinyser/StreamingProducerSpec.scala
+++ bitcoin-analyser/src/test/scala/coinyser/StreamingProducerSpec.scala
@@ -9,20 +9,20 @@ import StreamingProducerSpec._
 
 class StreamingProducerSpec extends WordSpec with Matchers with BeforeAndAfterAll with TypeCheckedTripleEquals with Eventually with EitherValues {
 
-  "KafkaProducer.deserializeWebsocketTransaction" should {
+  "StreamingProducer.deserializeWebsocketTransaction" should {
     "deserialize a valid String to a WebsocketTransaction" in {
       val str = """{"amount": 0.045318270000000001, "buy_order_id": 1969499130, "sell_order_id": 1969495276, "amount_str": "0.04531827", "price_str": "6339.73", "timestamp": "1533797395", "price": 6339.7299999999996, "type": 0, "id": 71826763}"""
       StreamingProducer.deserializeWebsocketTransaction(str) should ===(SampleWebsocketTransaction)
     }
   }
 
-  "KafkaProducer.convertTransaction" should {
+  "StreamingProducer.convertTransaction" should {
     "convert a WebSocketTransaction to a Transaction" in {
       StreamingProducer.convertWsTransaction(SampleWebsocketTransaction) should ===(SampleTransaction)
     }
   }
 
-  "KafkaProducer.serializeTransaction" should {
+  "StreamingProducer.serializeTransaction" should {
     "serialize a Transaction to a String" in {
       StreamingProducer.serializeTransaction(SampleTransaction) should ===(SampleJsonTransaction)
     }
