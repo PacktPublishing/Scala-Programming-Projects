ab826e25992d2e5d5b53291e6e5f972f1dce8b64
Mikael Valot <mikael.valot@gmail.com>
2018-09-15 15:41:02 +0100

    chapter3 - feedback

--- retirement-calculator/src/main/scala/retcalc/RetCalcError.scala
+++ retirement-calculator/src/main/scala/retcalc/RetCalcError.scala
@@ -2,28 +2,21 @@ package retcalc
 
 import cats.data.ValidatedNel
 
-sealed trait RetCalcError {
-  def message: String
-}
+sealed abstract class RetCalcError(val message: String)
 
 object RetCalcError {
   type RetCalcResult[A] = ValidatedNel[RetCalcError, A]
 
-  case class MoreExpensesThanIncome(income: Double, expenses: Double) extends RetCalcError {
-    override def message: String =
-      s"Expenses: $expenses >=  $income. You will never be able to save enough to retire !"
-  }
+  case class MoreExpensesThanIncome(income: Double, expenses: Double) extends RetCalcError(
+    s"Expenses: $expenses >=  $income. You will never be able to save enough to retire !")
 
-  case class ReturnMonthOutOfBounds(month: Int, maximum: Int) extends RetCalcError {
-    override def message: String =
-      s"Cannot get the return for month $month. Accepted range: 0 to $maximum"
-  }
+  case class ReturnMonthOutOfBounds(month: Int, maximum: Int) extends RetCalcError(
+    s"Cannot get the return for month $month. Accepted range: 0 to $maximum")
 
-  case class InvalidNumber(name: String, value: String) extends RetCalcError {
-    override def message: String = s"Invalid number for $name: $value"
-  }
+  case class InvalidNumber(name: String, value: String) extends RetCalcError(
+    s"Invalid number for $name: $value")
 
-  case class InvalidArgument(name: String, value: String, expectedFormat: String) extends RetCalcError {
-    override def message: String = s"Invalid format for $name. Expected: $expectedFormat, actual: $value"
-  }
+  case class InvalidArgument(name: String, value: String, expectedFormat: String) extends RetCalcError(
+    s"Invalid format for $name. Expected: $expectedFormat, actual: $value")
+
 }
